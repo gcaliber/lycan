77643e985b42b369ce62435b88092958b92a804b
Michael Green <mgreen1980@gmail.com>
2022-09-04 10:17:26 -0400

    installing basically works

--- src/lycan.nim
+++ src/lycan.nim
@@ -1,6 +1,8 @@
 import std/asyncdispatch
+import std/hashes
 import std/httpclient
 import std/json
+import std/jsonutils
 import std/strformat
 import std/strutils
 
@@ -30,12 +32,13 @@ type
     id: string
     source: AddonSource
     version: string
+    directories: seq[string]
 
 let config = parseJson(readFile("test/lycan.json"))
 let flavor = "retail"
 let tempDir = getTempDir()
 
-proc parseArg(arg: string): (string, AddonSource) =
+proc parseAddonUrl(arg: string): (string, AddonSource) =
   var matches: array[4, string]
   let pattern = re"^(?:https?:\/\/)?(?:www\.)?(.\w*)\.(?:com|org)\/(?:(?:(.\w*\/(.\w*-?\w*)(?:\.html)?))|(?:download\.php\?ui=(.*)))"
   discard find(arg, pattern, matches, 0, len(arg))
@@ -55,25 +58,113 @@ proc parseArg(arg: string): (string, AddonSource) =
 proc loadInstalledAddons(): seq[Addon] =
   let addonsJson = parseJson(readFile("test/lycan_addons.json"))
   var addons: seq[Addon]
-  for addon in addonsJson["addons"]:
+  for addon in addonsJson:
     addons.add(addon.to(Addon))
   return addons
 
-let addons: seq[Addon] = loadInstalledAddons()
-echo addons
+var addons: seq[Addon] = loadInstalledAddons()
+# var addons: seq[Addon]
+
+proc getLatestJson(url: string): Future[string] {.async.} =
+  var client = newAsyncHttpClient()
+  return await client.getContent(url)
+
+proc downloadAsset(url: string, filename: string) {.async.} =
+  var client = newAsyncHttpClient()
+  let response = await client.get(url)
+  let file = open(filename, fmWrite)
+  write(file, waitFor response.body)
+  close(file)
+
+proc unzip(filename: string, extractDir: string) =
+  var z: ZipArchive
+  if not z.open(filename):
+    echo fmt"Opening {filename} failed"
+    return
+  z.extractAll(extractDir)
+
+
+proc getAddonDirs(path: string): seq[string] =
+  var subDirs: seq[string] = @[path]
+  var dirs: seq[string] = @[]
+  while len(dirs) == 0 and len(subDirs) > 0:
+    let parent = subDirs[0]
+    for kind, path in walkDir(parent):
+      if kind == pcFile:
+        let (_, name, ext) = splitFile(path)
+        if ext == ".toc":
+          let (parentHead, parentTail) = splitPath(parent)
+          if name == parentTail:
+            return subDirs
+          else:
+            return @[joinPath(parentHead, name)]
+    subDirs = @[]
+    for kind, path in walkDir(parent):
+      if kind == pcDir:
+        subDirs.add(path)
+    # TODO: Error Handling, should raise an error instead of returning empty sequence
+  return @[]
+
+proc writeInstalledAddons() =
+  let options = ToJsonOptions(enumMode: joptEnumString, jsonNodeMode: joptJsonNodeAsRef)
+  let addonsJson = addons.toJson(opt = options)
+  let file = open("test/lycan_addons.json", fmWrite)
+  write(file, addonsJson)
+  close(file)
+
+proc installGithub(id: string) =
+  let latestUrl = fmt"https://api.github.com/repos/{id}/releases/latest"
+  let latestJson = parseJson(waitFor getLatestJson(latestUrl))
+  
+  let assets = latestJson["assets"]
+  var name: string
+  var downloadUrl: string
+  if len(assets) != 0:
+    for asset in assets:
+      if asset["content_type"].getStr() == "application/json":
+        continue
+      name = asset["name"].getStr()
+      let n = name.toLower()
+      if not (n.contains("bcc") or n.contains("tbc") or n.contains("wotlk") or n.contains("classic")):
+        downloadUrl = asset["browser_download_url"].getStr()
+        break
+  else:
+    name = hash(id).intToStr() & ".zip"
+    downloadUrl = latestJson["zipball_url"].getStr()
+    
+  let filename = joinPath(tempDir, name)
+  waitFor downloadAsset(downloadUrl, filename)
+  
+  let extractDir = joinPath(tempDir, name.strip(chars = {'z', 'i', 'p'}).strip(chars = {'.'}))
+  unzip(filename, extractDir)
+  
+  let sourceDirs = getAddonDirs(extractDir)
+  var addonsDirs: seq[string]
+  for dir in sourceDirs:
+    let (_, name) = splitPath(dir)
+    let destinationDir = joinPath(config[flavor]["dir"].getStr(), name)
+    moveDir(dir, destinationDir)
+    addonsDirs.add(destinationDir)
+  
+  var newAddon: Addon
+  newAddon.id = id
+  newAddon.source = github
+  let v = latestJson["name"].getStr()
+  newAddon.version = if v != "": v else: latestJson["tag_name"].getStr()
+  newAddon.directories = addonsDirs
+  
+  for addon in addons:
+    if addon.id == id:
+      addons.delete(addons.find(addon))
+      break
+  addons.add(newAddon)
+  writeInstalledAddons()
 
 proc installAddon(arg: string) =
-  let (id, source) = parseArg(arg)
+  let (id, source) = parseAddonUrl(arg)
   case source
     of github:
-      echo id, "  ", source
-      for addon in addons:
-        if addon.id == id:
-          return
-          #remove the directories and update the lycan_addons.json
-        else:
-          return
-          #add entry to lycan_addons.json
+      installGithub(id)
     else:
       quit()
 
@@ -98,77 +189,3 @@ for kind, key, val in opt.getopt():
 
 # default wow folder on windows C:\Program Files (x86)\World of Warcraft\
 # addons folder is <WoW>\_retail_\Interface\AddOns
-
-
-
-
-proc getLatestJson(url: string): Future[string] {.async.} =
-  var client = newAsyncHttpClient()
-  return await client.getContent(url)
-
-proc downloadAsset(url: string, filename: string) {.async.} =
-  var client = newAsyncHttpClient()
-  await client.downloadFile(url, filename)
-
-proc unzip(filename: string, extractDir: string) =
-  var z: ZipArchive
-  if not z.open(filename):
-    echo fmt"Opening {filename} failed"
-    return
-  z.extractAll(extractDir)
-
-proc getAddonDirs(path: string): seq[string] =
-  var subDirs: seq[string] = @[path]
-  var dirs: seq[string] = @[]
-  while len(dirs) == 0 and len(subDirs) > 0:
-    let current = subDirs[0]
-    for kind, path in walkDir(current):
-      if kind == pcFile:
-        let (_, _, ext) = splitFile(path)
-        if ext == ".toc":
-          return subDirs
-    subDirs = @[]
-    for kind, path in walkDir(current):
-      if kind == pcDir:
-        subDirs.add(path)
-    # TODO: Error Handling, should raise an error instead of returning empty sequence
-  return @[]
-
-proc updateAddon(addon: Addon, assets: JsonNode) =
-  for asset in assets:
-    if asset["content_type"].getStr() == "application/json":
-      continue
-    let name = asset["name"].getStr()
-    let nameLower = name.toLower()
-    if nameLower.contains("bcc") or 
-      nameLower.contains("tbc") or
-      nameLower.contains("wotlk") or
-      nameLower.contains("classic"):
-        continue
-    else:
-      let downloadUrl = asset["browser_download_url"].getStr()
-      let filename = joinPath(tempDir, name)
-      waitFor downloadAsset(downloadUrl, filename)
-      
-      let extractDir = joinPath(tempDir, name.strip(chars = {'z', 'i', 'p'}).strip(chars = {'.'}))
-      unzip(filename, extractDir)
-      
-      let addonDirs = getAddonDirs(extractDir)
-      for dir in addonDirs:
-        let (_, name) = splitPath(dir)
-        let destinationDir = joinPath(config[flavor]["dir"].getStr(), name)
-        moveDir(dir, destinationDir)
-
-
-      break
-  return
-
-for addon in addons:
-  let latestUrl = fmt"https://api.github.com/repos/{addon.id}/releases/latest"
-  let latestJson = parseJson(waitFor getLatestJson(latestUrl))
-
-  let version = latestJson["name"].getStr()
-  if version == addon.version:
-    continue
-  else:
-    updateAddon(addon, latestJson["assets"])
